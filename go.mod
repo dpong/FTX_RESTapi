8f6f67c80633fb75bb6c3326309a639579692010
powenperng <powen@injectiveprotocal.com>
2022-03-28 17:33:00 +0800

    add trade stream features.

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/dpong/FTX_RESTapi
+
+go 1.17
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/json-iterator/go v1.1.12
+	github.com/shopspring/decimal v1.3.1
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
---
+++ streamMarketTrades.go
@@ -0,0 +1,184 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/gorilla/websocket"
+)
+
+type StreamMarketTradesBranch struct {
+	cancel *context.CancelFunc
+	conn   *websocket.Conn
+	market string
+
+	tradeChan    chan FTXTradeData
+	tradesBranch struct {
+		Trades []FTXTradeData
+		sync.Mutex
+	}
+	logger *logrus.Logger
+}
+
+type FTXTradeData struct {
+	Channel string `json:"channel"`
+	Market  string `json:"market"`
+	Type    string `json:"type"`
+	Data    []struct {
+		ID          int64     `json:"id"`
+		Price       float64   `json:"price"`
+		Size        float64   `json:"size"`
+		Side        string    `json:"side"`
+		Liquidation bool      `json:"liquidation"`
+		Time        time.Time `json:"time"`
+	} `json:"data"`
+}
+
+func TradeStream(symbol string, logger *logrus.Logger) *StreamMarketTradesBranch {
+	Usymbol := strings.ToUpper(symbol)
+	return tradeStream(Usymbol, logger)
+}
+
+// side: Side of the taker in the trade
+func (o *StreamMarketTradesBranch) GetTrades() []FTXTradeData {
+	o.tradesBranch.Lock()
+	defer o.tradesBranch.Unlock()
+	trades := o.tradesBranch.Trades
+	o.tradesBranch.Trades = []FTXTradeData{}
+	return trades
+}
+
+func tradeStream(symbol string, logger *logrus.Logger) *StreamMarketTradesBranch {
+	o := new(StreamMarketTradesBranch)
+	ctx, cancel := context.WithCancel(context.Background())
+	o.cancel = &cancel
+	o.market = symbol
+	o.tradeChan = make(chan FTXTradeData, 100)
+	o.logger = logger
+	go o.maintainSession(ctx, symbol)
+	go o.listen(ctx)
+	return o
+}
+
+func (o *StreamMarketTradesBranch) listen(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case trade := <-o.tradeChan:
+			o.appendNewTrade(&trade)
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func (o *StreamMarketTradesBranch) appendNewTrade(new *FTXTradeData) {
+	o.tradesBranch.Lock()
+	defer o.tradesBranch.Unlock()
+	o.tradesBranch.Trades = append(o.tradesBranch.Trades, *new)
+}
+
+func (o *StreamMarketTradesBranch) maintainSession(ctx context.Context, symbol string) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := o.maintain(ctx, symbol); err == nil {
+				return
+			} else {
+				o.logger.Warningf("reconnect FTX %s trade stream with err: %s\n", symbol, err.Error())
+			}
+		}
+	}
+}
+
+func (o *StreamMarketTradesBranch) maintain(ctx context.Context, symbol string) error {
+	var duration time.Duration = 30
+	url := "wss://ftx.com/ws/"
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return err
+	}
+	o.conn = conn
+	defer o.conn.Close()
+	if send, err := o.getSubscribeMessage(); err != nil {
+		return err
+	} else {
+		if err := o.conn.WriteMessage(websocket.TextMessage, send); err != nil {
+			return err
+		}
+	}
+	if err := o.conn.SetReadDeadline(time.Now().Add(time.Second * duration)); err != nil {
+		return err
+	}
+	o.conn.SetPingHandler(nil)
+	pingpong := time.NewTicker(time.Second * 15)
+	defer pingpong.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-pingpong.C:
+			sub := fTXSubscribeMessage{Op: "ping"}
+			message, err := json.Marshal(sub)
+			if err != nil {
+				return err
+			}
+			if err := o.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				o.conn.SetReadDeadline(time.Now().Add(time.Second))
+				return err
+			}
+			o.conn.SetReadDeadline(time.Now().Add(time.Second * duration))
+		default:
+			_, msg, err := o.conn.ReadMessage()
+			if err != nil {
+				return err
+			}
+			if err := o.handleFTXTradeSocketMsg(msg); err != nil {
+				return err
+			}
+			if err := o.conn.SetReadDeadline(time.Now().Add(time.Second * duration)); err != nil {
+				return err
+			}
+		} // end select
+	} // end for
+}
+
+func (o *StreamMarketTradesBranch) handleFTXTradeSocketMsg(msg []byte) error {
+	var data FTXTradeData
+	err := json.Unmarshal(msg, &data)
+	if err != nil {
+		return errors.New("fail to unmarshal message")
+	}
+	switch data.Channel {
+	case "trades":
+		switch data.Type {
+		case "update":
+			o.tradeChan <- data
+		case "subscribed":
+			o.logger.Infof("Connected FTX %s trade stream.", o.market)
+		default:
+			// error and info
+			return err
+		}
+	default:
+		// ping pong
+	}
+	return nil
+}
+
+func (o *StreamMarketTradesBranch) getSubscribeMessage() ([]byte, error) {
+	sub := fTXSubscribeMessage{Op: "subscribe", Channel: "trades", Market: o.market}
+	message, err := json.Marshal(sub)
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
+}
